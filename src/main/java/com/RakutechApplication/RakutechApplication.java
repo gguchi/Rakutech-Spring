0d7bfc76022cabd2275707cbb03aa4ffac0081a9
Norimar Iguchi <gguchi193@gmail.com>
2021-06-24 19:43:09 +0800

    Implementing controller for the app

--- src/main/java/com/RakutechApplication/RakutechApplication.java
+++ src/main/java/com/RakutechApplication/RakutechApplication.java
@@ -1,4 +1,4 @@
-package com.example.Rakutech;
+package com.RakutechApplication;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
